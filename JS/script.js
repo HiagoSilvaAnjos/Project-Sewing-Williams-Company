74df2ca900685c75066354706fa84cafb783020c
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-01-02 20:02:42 -0300

    melhorias no script

--- JS/script.js
+++ JS/script.js
@@ -1,25 +1,35 @@
 const elementContainerNav = document.querySelector('.container-inner-content');
 const buttonShowNav = document.querySelector('.menu-button');
+const listelementsContainer = document.querySelector('.list-elements');
+
+let state = 'menu-off';
+
+
+listelementsContainer.addEventListener('click', (e) => {
+    const element = e.target;
+
+    if (element.classList.contains('list-links')) {
+        hiddenSectionNav();
+    }
+
+})
 
 buttonShowNav.addEventListener('click', () => {
+
+    if (state === 'menu-off') {
+        buttonShowNav.innerText = 'close';
+        state = 'menu-on'
+    } else {
+        buttonShowNav.innerText = 'menu';
+        state = 'menu-off'
+    }
+
     elementContainerNav.classList.toggle('show-menu');
 })
 
+
 function hiddenSectionNav() {
     elementContainerNav.classList.remove('show-menu');
+    buttonShowNav.innerText = 'menu';
+    state = 'menu-off';
 }
-
-const initialSectionLink = document.querySelector('#initialSectionLink');
-initialSectionLink.addEventListener('click', () => hiddenSectionNav());
-
-const menSectionLink = document.querySelector('#menSectionLink');
-menSectionLink.addEventListener('click', () => hiddenSectionNav());
-
-const feSectionLink = document.querySelector('#feSectionLink');
-feSectionLink.addEventListener('click', () => hiddenSectionNav());
-
-const aboutSectionLink = document.querySelector('#aboutSectionLink');
-aboutSectionLink.addEventListener('click', () => hiddenSectionNav());
-
-const tecSectionLink = document.querySelector('#tecSectionLink');
-tecSectionLink.addEventListener('click', () => hiddenSectionNav());
