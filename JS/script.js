993c10117e7a5fe4f234b743a50a1000d9e09121
Hiago Silva <hiagosilvaanjos7@gmail.com>
2024-01-02 08:40:08 -0300

    update

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,22 @@ const buttonShowNav = document.querySelector('.menu-button');
 buttonShowNav.addEventListener('click', () => {
     elementContainerNav.classList.toggle('show-menu');
 })
+
+function hiddenSectionNav() {
+    elementContainerNav.classList.remove('show-menu');
+}
+
+const initialSectionLink = document.querySelector('#initialSectionLink');
+initialSectionLink.addEventListener('click', () => hiddenSectionNav());
+
+const menSectionLink = document.querySelector('#menSectionLink');
+menSectionLink.addEventListener('click', () => hiddenSectionNav());
+
+const feSectionLink = document.querySelector('#feSectionLink');
+feSectionLink.addEventListener('click', () => hiddenSectionNav());
+
+const aboutSectionLink = document.querySelector('#aboutSectionLink');
+aboutSectionLink.addEventListener('click', () => hiddenSectionNav());
+
+const tecSectionLink = document.querySelector('#tecSectionLink');
+tecSectionLink.addEventListener('click', () => hiddenSectionNav());
